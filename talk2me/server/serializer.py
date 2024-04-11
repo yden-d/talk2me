7d7a05e2b509953d67906a98516d9dd40e38ba1d
Yden Da <yden.business@gmail.com>
2024-04-11 13:36:57 -0400

    Server serializer adjustments and more API filtering implementations

--- talk2me/server/serializer.py
+++ talk2me/server/serializer.py
@@ -1,13 +1,29 @@
 from rest_framework import serializers
-from .models import Server, Category
+from .models import Server, Channel
 
 
-class ServerSerializer(serializers.ModelSerializer):
+class ChannelSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Server
+        model = Channel
         fields = "__all__"
 
 
-class CategorySerializer:
-    pass
+class ServerSerializer(serializers.ModelSerializer):
+    num_members = serializers.SerializerMethodField()
+    channel_server = ChannelSerializer(many=True)
+
+    class Meta:
+        model = Server
+        exclude = ("member",)
+    
+    def get_num_members(self, obj):
+        if hasattr(obj, "num_members"):
+            return obj.num_members
+        return None
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        num_members = self.context.get("num_members")
+        data.pop("num_members") if not num_members else data
+        return data
--- talk2me/server/views.py
+++ talk2me/server/views.py
@@ -1,6 +1,13 @@
+# Django Imports
 from django.shortcuts import render
+from django.db.models import Count
+
+# DRF Imports
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
+
+# Internal Imports
 from .models import Server, Category, Channel
 from .serializer import ServerSerializer
 
@@ -11,9 +18,38 @@ class ServerListViewSet(viewsets.ViewSet):
     def list(self, request):
         category = request.query_params.get("category")
         category_name = request.query_params.get("category_name")
+        by_user = request.query_params.get("by_user") == "True"
+        qty = request.query_params.get("qty")
+        server_id = request.query_params.get("server_id")
+        num_members = request.query_params.get("with_num_members") == "True"
 
+        if by_user or server_id and not request.user.is_authenticated:
+            raise AuthenticationFailed()
+
+        filters = {}
         if category:
-            self.queryset.filter(category=category, category__name=category_name)
+            filters["category"] = category
+        if category_name:
+            filters["category__name"] = category_name
+        if by_user:
+            filters["by_user"] = request.user.id
+        if server_id:
+            try:
+                server_id = int(server_id)
+                if not Server.objects.filter(id=server_id).exists():
+                    raise ValidationError(detail=f"Server with id {server_id} does not exist.")
+                filters["id"] = server_id
+            except ValueError:
+                raise ValidationError(detail="Invalid server id format.")
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        if num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
+
+        if filters:
+            self.queryset = self.queryset.filter(**filters)
+
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
+
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members": num_members})
         return Response(serializer.data)
